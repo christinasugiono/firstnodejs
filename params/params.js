fe1c849226a7c983cae61b7c4d93164d808418b4
christinasugiono <74747669+christinasugiono@users.noreply.github.com>
2021-05-17 21:04:26 +0700

    my first app part 3

---
+++ params/params.js
@@ -0,0 +1,5 @@
+const DATABASECONNECTION = "mongodb+srv://rootadmin:pass123@cluster0.z4j0i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+module.exports = {
+    DATABASECONNECTION
+}
--- routes/web/index.js
+++ routes/web/index.js
@@ -1,5 +1,12 @@
-var express = require("express")
+var express = require("express");
+
 var router = express.Router();
+
+
+router.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 router.use("/", require("./home"));
 
